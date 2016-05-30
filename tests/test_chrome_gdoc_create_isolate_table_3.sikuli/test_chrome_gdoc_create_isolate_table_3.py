3a6bef8748817f662a35ce6ac6c7e254ee846201
Farmer Tseng <fatseng@mozilla.com>
2016-05-30 14:10:22 +0800

    create 3 and 10 isolate table

---
+++ tests/test_chrome_gdoc_create_isolate_table_3.sikuli/test_chrome_gdoc_create_isolate_table_3.py
@@ -0,0 +1,13 @@
+# if you are putting your test script folders under {git project folder}/tests/, it will work fine.
+# otherwise, you either add it to system path before you run or hard coded it in here.
+sys.path.append(sys.argv[2])
+import gdoc
+
+gd = gdoc.gDoc()
+gd.focus_content()
+for i in range(3):
+    gd.create_table(10,5)
+    type(Key.ENTER)
+    type(Key.ENTER)
+
+gd.deFoucsContentWindow()
